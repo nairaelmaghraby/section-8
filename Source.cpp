3071e6be21fc7636928085e03bbe7d23651db042
naira <nairaelmaghraby@gmail.com>
2021-10-20 09:44:37 +0200

    first commit

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int no1, no2, sum;
+		cout << "Enter two integers: ";
+		cin >> no1 >> no2;
+		sum = no1 + no2;
+		cout << no1 << "+" << no2 << "=" << sum;
+		return 0;
+}
